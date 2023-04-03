d34c130147fcf15d4e140e6bbe0a0eb7702a676a
Kalesh Singh <kaleshsingh@google.com>
2023-04-03 15:28:17 -0700

    ANDROID: MGLRU: Avoid reactivation of anon pages on swap full
    Avoid anon reclaim if swapping full since this reactivates the
    pages.
    
    Bug: 276804472
    Bug: 261619133
    Bug: 276521916
    Change-Id: Ia3af7fe8d5b29405830a812e73f95d11a0f8ee3a
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2636,6 +2636,9 @@ static int get_swappiness(struct lruvec *lruvec, struct scan_control *sc)
 		return 0;
 	}
 
+	if (mem_cgroup_get_nr_swap_pages(memcg) <= 0)
+		return 0;
+
 	return mem_cgroup_swappiness(memcg);
 }
 
