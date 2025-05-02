68915e25ecc1389c590c2780733963cf38f40b97
chickendrop89 <chickendrop89@gmail.com>
2025-05-02 22:58:27 +0200

    Revert "ANDROID: MGLRU: Avoid reactivation of anon pages on swap full"
    This reverts commit 3f1a8ee639e04a36c6b21b79d95af6fc3723e19f.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2631,10 +2631,9 @@ static int get_swappiness(struct lruvec *lruvec, struct scan_control *sc)
 {
 	struct mem_cgroup *memcg = lruvec_memcg(lruvec);
 
-	if (mem_cgroup_get_nr_swap_pages(memcg) <= 0) {
+	if (mem_cgroup_get_nr_swap_pages(memcg) < MIN_LRU_BATCH) {
 		count_vm_event(current_is_kswapd() ? LRU_KSWAPD_SWAP_FULL:
 						     LRU_DIRECT_SWAP_FULL);
-		return 0;
 	}
 
 	if (mem_cgroup_get_nr_swap_pages(memcg) <= 0)
